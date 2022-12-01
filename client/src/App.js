1ed4a68a152b61c551d1123b9798ec524177510c
Ricardo Suarez <ricardo_suarez93@hotmail.com>
2022-11-30 22:20:50 -0500

    prueba (#4)
    Co-authored-by: facundo-monllor <103942226+facundo-monllor@users.noreply.github.com> GODDD

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,16 @@ function App() {
   return (
     <div className="App">
       <h1>Cripto Bros</h1>
+      <h1>Prueba 3</h1>
+      
       <BrowserRouter>
         <Route exact path={"/"} component={LandingPage} />
       </BrowserRouter>
+
+      <BrowserRouter>
+        <Route exact path={"/"} component={LandingPage} />
+      </BrowserRouter>
+      
     </div>
   );
 }
