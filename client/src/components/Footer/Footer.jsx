0cc843cf5e2fcc98d457850cf1ec615c52bbf761
Ricardo Suarez <ricardo_suarez93@hotmail.com>
2022-12-01 00:22:53 -0500

    Update

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import IMG from '../../Images/logoPrueba.png';
 
 const Footer = () => {
   return (
     <div>
-      <h1>footer</h1>
+      <hr />
+      <img src={IMG} alt='LOGO' />
+      <h1>Todo por 2 criptos</h1>
+      <h1>Derechos Reservados</h1>
+      <div>
+        <h2>CONTACTO</h2>
+        <h3>+54 1178434342</h3>
+        <h3>todox2criptos@gmail.com</h3>
+        <h3>Av. Cabildo 1994</h3>
+      </div>      
+      <Link to='/FAQ'><h2>FAQ</h2></Link>
+      <Link><h2>DEVELOPERS</h2></Link>
+      <Link><h2>ABOUT</h2></Link>
     </div>
   )
 }
