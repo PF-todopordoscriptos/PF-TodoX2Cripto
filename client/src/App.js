5b7af0198d1e0af123f64b0708a74a586066f521
Ricardo Suarez <ricardo_suarez93@hotmail.com>
2022-11-30 22:10:37 -0500

    prueba

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,14 @@ function App() {
   return (
     <div className="App">
       <h1>Cripto Bros</h1>
+<<<<<<< Updated upstream
+=======
+      <h1>Prueba 3</h1>
+      
+      <BrowserRouter>
+        <Route exact path={"/"} component={LandingPage} />
+      </BrowserRouter>
+>>>>>>> Stashed changes
     </div>
   );
 }
