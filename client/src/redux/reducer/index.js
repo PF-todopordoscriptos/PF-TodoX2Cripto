d57dd9c3ec695343f1e93ccf03a8cce0fa636ac7
Juan Cruz <juancruzperez@gmail.com>
2022-11-30 22:29:48 -0300

    starting landingComponent

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,8 +1,20 @@
+import { GET_TRENDING_COINS } from "../actions/actionTypes";
+
 const initialState = {
   allCoins: [],
   coinDetails: {},
   trendingCoins: [],
 };
 
-function rootReducer(state = initialState, action) {}
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_TRENDING_COINS:
+      return {
+        ...state,
+        trendingCoins: action.payload,
+      };
+    default:
+      return state;
+  }
+}
 export default rootReducer;
