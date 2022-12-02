f3b80e4f3aa43b0b268814efd8473f0019f67e53
LifThiago <lifthiago@gmail.com>
2022-12-01 22:23:15 -0300

    Created CARD comp
    Created CARD component and added HOME component the cards list

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllCoins } from "../../redux/actions";
+import Card from "../Card/Card";
 
-const Home = () => {
-  return <div>Home</div>;
-};
+export default function Home() {
 
-export default Home;
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getAllCoins())
+  }, [])
+
+  const allCoins = useSelector((state) => state.allCoins)
+  console.log(allCoins)
+
+  return (
+    <div>
+        {allCoins && 
+        allCoins.map((c) => {
+          return <Card
+                  id= {c.id}
+                  name= {c.name}
+                  symbol= {c.symbol}
+                  current_price= {c.current_price}
+                  image= {c.image}
+                  market_cap= {c.market_cap}
+                  price_change_percentage_24h= {c.price_change_percentage_24h}
+                  />
+        })
+        }
+    </div>
+  )
+}
+
