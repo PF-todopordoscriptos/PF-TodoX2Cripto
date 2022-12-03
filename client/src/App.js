f78c19104dde7ab4c5d024d65e55666f23544df7
Ricardo Suarez <ricardo_suarez93@hotmail.com>
2022-12-03 03:23:26 -0500

    Cambios minimos

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,41 @@ import FAQ from "./components/FAQ/FAQ.jsx";
 import Chart from "./components/Chart/Chart";
 import Home from "./components/Home/Home";
 import Details from "./components/Details/Details";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      "Zen Dots"
+    ].join(",")
+  },
+  palette:{
+    primary: {
+      main: "#0052cc",
+      sub: "#F5E024",
+      dark: "#1E1E1E",
+    },
+    secondary: {
+      main: '#edf2ff',
+    },
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <h1>TODO POR 2 CRIPTO</h1>
-      <BrowserRouter>
-        <Route exact path={"/"} component={LandingPage} />
-        <Route exact path={"/FAQ"} component={FAQ} />
-        <Route exact path={["/", "/FAQ"]} component={Footer} />
-        <Route path={"/chart/:id"} component={Chart} />
-        <Route path={"/details/:id"} component={Details} />
-        <Route path={"/home"} component={Home} />
-      </BrowserRouter>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <h1>TODO POR 2 CRIPTO</h1>
+        <BrowserRouter>
+          <Route exact path={"/"} component={LandingPage} />
+          <Route exact path={"/FAQ"} component={FAQ} />
+          <Route exact path={["/", "/FAQ"]} component={Footer} />
+          <Route path={"/chart/:id"} component={Chart} />
+          <Route path={"/details/:id"} component={Details} />
+          <Route path={"/home"} component={Home} />
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,10 +1,95 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import IMG from '../../Images/logoPrueba.png';
+import React, { Fragment } from "react";
+//import { Link } from "react-router-dom";
+import IMG from "../../Images/logoPrueba.png";
+
+import { Stack, CardMedia, Typography, Link  } from "@mui/material";
+
+
 
 const Footer = () => {
   return (
-    <div>
+    <Fragment>
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+        sx={{
+          bgcolor: "primary.dark",
+          color: "secondary.main",
+        }}
+      >
+        <Stack
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={25}
+        >
+          <Stack>
+            <CardMedia
+              component="img"
+              // height="20"
+              // width="35"
+              image={IMG}
+              alt="logoPrueba"
+              sx={{
+                mt: "20px",
+              }}
+            />
+          </Stack>
+          <Stack
+            sx={{
+              
+              fontWeight: 'bold'
+            }}
+            >
+            <Typography variant="subtitle1" gutterBottom>
+              CONTACTO
+            </Typography>
+            <Typography variant="subtitle2" gutterBottom>
+              +54 1178434342
+            </Typography>
+            <Typography variant="subtitle2" gutterBottom>
+              todox2criptos@gmail.com
+            </Typography>
+            <Typography variant="subtitle2" gutterBottom>
+              Av. Cabildo 1994
+            </Typography>
+          </Stack> 
+          <Stack>
+            <Link href="/FAQ" underline="none">
+              <Typography variant="subtitle1" gutterBottom>
+                FAQ
+              </Typography>
+            </Link>
+            <Link href="#" underline="none">
+              <Typography variant="subtitle1" gutterBottom>
+                ABOUT
+              </Typography>
+            </Link>
+            <Link href="#" underline="none">
+              <Typography variant="subtitle1" gutterBottom>
+                DEVELOPERS
+              </Typography>
+            </Link>
+          </Stack>
+        </Stack>
+        <Stack>
+          <Typography variant="subtitle1" gutterBottom>
+            Todo por 2 criptos©
+          </Typography>
+          <Typography variant="subtitle1" gutterBottom>
+            Derechos Reservados
+          </Typography>
+        </Stack>
+      </Stack>
+    </Fragment>
+  );
+};
+
+export default Footer;
+/*
+
       <hr />
       <img src={IMG} alt='LOGO' />
       <h1>Todo por 2 criptos</h1>
@@ -18,8 +103,4 @@ const Footer = () => {
       <Link to='/FAQ'><h2>FAQ</h2></Link>
       <Link><h2>DEVELOPERS</h2></Link>
       <Link><h2>ABOUT</h2></Link>
-    </div>
-  )
-}
-
-export default Footer
+ */
