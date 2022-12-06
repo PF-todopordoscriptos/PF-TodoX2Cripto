a27816bc3ebac346dd209909cb2fd5b59353ded0
Juan Cruz <juancruzperez@gmail.com>
2022-12-06 18:35:32 -0300

    CHART-JuanCruz

--- api/src/models/Coins.js
+++ api/src/models/Coins.js
@@ -3,12 +3,16 @@ const { DataTypes } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("coins", {
-    name: {
-      type: DataTypes.STRING,
-      //allowNull: false,
+  sequelize.define(
+    "coins",
+    {
+      name: {
+        type: DataTypes.STRING,
+        //allowNull: false,
+      },
     },
-  }, {
-    timestamps: false
-  });
+    {
+      timestamps: true,
+    }
+  );
 };
