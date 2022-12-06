ceb783cf199ed0cfc3de79b5f0da526281e20a93
facundo-monllor <facundomonllor@gmail.com>
2022-12-06 20:27:30 -0300

    login-facu
    cambie el login para que se abra una ventana emergente

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { useAuth0 } from "@auth0/auth0-react"
 
 const Login = () => {
-    const {loginWithRedirect} = useAuth0()
+    const {loginWithPopup} = useAuth0()
 
 
   return (
     <div>
-        <button onClick={() => loginWithRedirect()}>
+        <button onClick={() => loginWithPopup()}>
             Login
         </button>
     </div>
