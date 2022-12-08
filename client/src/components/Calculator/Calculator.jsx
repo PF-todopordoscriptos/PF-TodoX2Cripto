2321ba6e6c494613afc1208a6c30119c9b7782d8
Juan Cruz <juancruzperez@gmail.com>
2022-12-07 23:03:25 -0300

    CALCULATOR-JuanCruz
    corrections to calculator

--- client/src/components/Calculator/Calculator.jsx
+++ client/src/components/Calculator/Calculator.jsx
@@ -14,27 +14,63 @@ const Calculator = () => {
   console.log(allCoins);
 
   const [coin, setCoin] = useState({});
+  const [price, setPrice] = useState("");
+
+  const renderPriceCoin = () => {
+    let result;
+    let coinSelected = allCoins.find((p) => p.id === coin);
+    Object.keys(coin).length === 0
+      ? (result = "Select one coin to start comparation")
+      : (result = `Price: USD$ ${coinSelected.current_price}`);
+    return result;
+  };
+
+  const renderCalculatedPrice = () => {
+    let result;
+    let coinSelected = allCoins.find((p) => p.id === coin);
+    result = coinSelected.current_price;
+    console.log(result);
+    return result;
+  };
 
   const handleSelectedCoins = (e) => {
     e.preventDefault();
-    setCoin("");
-    console.log(e.target.value);
+    console.log(e.target);
+    setCoin(e.target.value);
   };
+
+  function handleInput(e) {
+    e.preventDefault();
+    setPrice(e.target.value);
+  }
 
   return (
     <div>
-      <label>Coin Exchange</label>
-      <select onChange={(e) => handleSelectedCoins(e)}>
-        <option value="none">None</option>
-        {allCoins &&
-          allCoins.map((c) => {
-            return (
-              <option key={c.id} value={c.id}>
-                {c.id}
-              </option>
-            );
-          })}
-      </select>
+      <div>
+        <label>Coin Exchange</label>
+        <select onChange={(e) => handleSelectedCoins(e)}>
+          <option value="none">None</option>
+          {allCoins &&
+            allCoins.map((c) => {
+              return (
+                <option key={c.id} value={c.id}>
+                  {c.id}
+                </option>
+              );
+            })}
+        </select>
+        <label>{renderPriceCoin()}</label>
+      </div>
+      <div>
+        <label>Buy:</label>
+        <input
+          type="number"
+          placeholder="Select amount"
+          value={price}
+          onChange={(e) => handleInput(e)}
+        />
+        <label>{renderCalculatedPrice}</label>
+      </div>
     </div>
   );
 };
