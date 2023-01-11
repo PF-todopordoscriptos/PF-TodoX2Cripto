775fa73b9a5a4ad13910f3983629df7fcc34b9dd
facundo-monllor <facundomonllor@gmail.com>
2023-01-11 16:58:34 -0300

    wallet-ramifacu
    wallet-ramifacu faltan cosas

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,38 +1,28 @@
 import React from "react";
 import { Box, Paper, Typography } from "@mui/material";
 
+import style from "./About.module.css";
+
 const About = () => {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "center",
-        alignItems: "center",
-        "& > :not(style)": {
-          m: 10,
-          width: 855,
-          height: 455,
-        },
-      }}
-    >
-      <Paper
-        elevation={5}
-        sx={{
-          backgroundColor: "rgb(168, 135, 187)",
-          border: "2px solid #684477",
-          borderRadius: "0.8rem",
-        }}
-      >
-        <Typography variant="h2" gutterBottom sx={{ margin: "1rem" }}>
-          About
-        </Typography>
-        <Typography variant="h4" gutterBottom sx={{ margin: "1rem" }}>
-          This is the final project of SoyHenry's programming bootcamp. It is a
-          web application for buying and selling cryptocurrencies.
-        </Typography>
-      </Paper>
-    </Box>
+    <div className={style.contTodo}>
+
+      <div className={style.contTitulo}>
+        <h1 className={style.Titulo}>About</h1>
+      </div>
+
+      <div className={style.contMedio}>
+      <div className={style.contP}>
+        <p className={style.pp}>Todo x 2 Criptos es una compañía fundada en 2.022 especializada en tecnología financiera. Con un profundo conocimiento de la tecnología Blockchain y de las criptomonedas. Ayudamos a individuos a acceder, comerciar y gestionar criptomonedas y activos digitales de forma óptima.</p>
+        <p className={style.pp}>Siendo la puerta de acceso a mercados sin fricciones, dejando obsoleto el sistema financiero tradicional, la compañía está formada 7 personas: idealistas, emprendedores tecnológicos, investigadores, traders cuantitativos, financieros, e ingenieros.</p>
+        <p className={style.pp}>Operamos sin descanso alrededor de todas las fases horarias y de todo el mundo, con oficinas centrales en Argentina, America del Sur.</p>
+      </div>
+      <div className={style.contDino}>
+        <img src="https://res.cloudinary.com/dpb5vf1q1/image/upload/v1671219807/cripto/pbyyyfclacsoq3hyrcs9.png" alt="dino" className={style.dino}/>
+      </div>
+      </div>
+
+    </div>
   );
 };
 
