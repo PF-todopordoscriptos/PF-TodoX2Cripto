f130e9827c2c7f2e57409256d240aa2723be2fab
RodriAppel <rodrigoappel_13@hotmail.com>
2023-01-11 18:53:23 -0300

    Ajuustes

--- client/src/components/FAQ/FAQ.jsx
+++ client/src/components/FAQ/FAQ.jsx
@@ -12,11 +12,12 @@ import { HiChevronDown } from "react-icons/hi2";
 
 const Accordion = withStyles({
   root: {
+    color: "white",
     width: "70%",
-    backgroundColor: "rgb(168, 135, 187)",
+    backgroundColor: " #7b27a5;",
     border: "2px solid #684477",
     margin: "0.7rem",
-    borderRadius: "0.2rem",
+    borderRadius: "8px",
   },
   expanded: {},
 })(MuiAccordion);
@@ -39,71 +40,6 @@ const AccordionDetails = withStyles({
 
 const FAQ = () => {
   return (
-    // <div className={style.cont}>
-    // <Box
-    //   display="flex"
-    //   justifyContent="center"
-    //   alignItems="center"
-    //   >
-    //   <Box
-    //     sx={{
-    //       border: "1px solid primary.dark",
-    //       borderRadius:"15px",
-    //       backgroundColor: "primary.dark",
-    //       width: "600px",
-    //       height: "400px",
-    //     }}
-    //   >
-    //     <Typography sx={{ color: "secondary.main" }} variant="h3" gutterBottom>
-    //       Frequently Asked Questions
-    //     </Typography>
-    //     <Stack>
-    //       <Typography sx={{ color: "primary.sub" }} variant="h5" gutterBottom>
-    //         ¿Puedo comprar sin registrarme?
-    //       </Typography>
-    //       <Typography
-    //         sx={{ color: "secondary.main" }}
-    //         variant="subtitle2"
-    //         gutterBottom
-    //       >
-    //         Para hacer una compra es necesario tener una cuenta.
-    //       </Typography>
-    //       <Typography sx={{ color: "primary.sub" }} variant="h5" gutterBottom>
-    //         ¿Hay minimo de edad?
-    //       </Typography>
-    //       <Typography
-    //         sx={{ color: "secondary.main" }}
-    //         variant="subtitle2"
-    //         gutterBottom
-    //       >
-    //         Es necesario ser mayor de 18 años.
-    //       </Typography>
-    //       <Typography sx={{ color: "primary.sub" }} variant="h5" gutterBottom>
-    //         ¿Que metodos de pago hay?
-    //       </Typography>
-    //       <Typography
-    //         sx={{ color: "secondary.main" }}
-    //         variant="subtitle2"
-    //         gutterBottom
-    //       >
-    //         Trabajamos con la pasarela de pago de Mercado Pago
-    //       </Typography>
-    //       <Typography sx={{ color: "primary.sub" }} variant="h5" gutterBottom>
-    //         ¿Hay devoluciones?
-    //       </Typography>
-    //       <Typography
-    //         sx={{ color: "secondary.main" }}
-    //         variant="subtitle2"
-    //         gutterBottom
-    //       >
-    //         Una vez que la cripto comprada entro a tu billetera no hay forma de
-    //         volver atras.
-    //       </Typography>
-    //     </Stack>
-    //   </Box>
-    // </Box>
-    // </div>
-
     <div className={style.cont}>
       <div className="accordions">
         <Accordion>
