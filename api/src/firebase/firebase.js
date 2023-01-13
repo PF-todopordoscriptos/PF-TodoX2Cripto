e154a225d18486632f62314443cc68159236e9a7
Ricardo Suarez <ricardo_suarez93@hotmail.com>
2023-01-13 13:27:47 -0500

    Update firebase.js

--- api/src/firebase/firebase.js
+++ api/src/firebase/firebase.js
@@ -2,7 +2,7 @@ const { initializeApp, applicationDefault } = require("firebase-admin/app");
 const admin = require("firebase-admin");
 const { getAuth } = require("firebase-admin/auth");
 //const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS)
-const serviceAccount = require("/etc/secrets/GOOGLE_APPLICATION_CREDENTIALS");
+//const serviceAccount = require("/etc/secrets/GOOGLE_APPLICATION_CREDENTIALS");
 
 
 const firebaseConfig = {
